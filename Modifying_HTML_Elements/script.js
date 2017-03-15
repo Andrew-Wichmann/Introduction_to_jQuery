26b2c9f6f104f8b1b5d1bc4f5bd6df8819c34a5d
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-03-14 21:10:03 -0500

    added some equivical .css() calls that are commented out.

--- Modifying_HTML_Elements/script.js
+++ Modifying_HTML_Elements/script.js
@@ -2,4 +2,9 @@
     $('div').height("200px");
     $('div').width("200px");
     $('div').css("border-radius", "10px");
- });
+    
+/*
+    $('div').css("height", "200px");
+    $('div').css("width", "200px");
+    $('div').css("border-radius", "10px");*/
+});
