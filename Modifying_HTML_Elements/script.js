e82044e8398a2ffe31308e73dfdef0c64697f813
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-03-27 20:28:44 -0500

    Added a click to add css feature

--- Modifying_HTML_Elements/script.js
+++ Modifying_HTML_Elements/script.js
@@ -2,9 +2,7 @@
     $('div').height("200px");
     $('div').width("200px");
     $('div').css("border-radius", "10px");
-    
-/*
-    $('div').css("height", "200px");
-    $('div').css("width", "200px");
-    $('div').css("border-radius", "10px");*/
+    $('div').click(function(){
+      $('div').css("background-color", "red");
+    })
 });
